091d8832ecbb4d2095d52a61a8a8c3a64c9ddd3e
Jividha <jividha11@gmail.com>
2026-02-20 14:14:16 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+console.log("JWT SECRET =", process.env.JWT_SECRET);
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors"); 
+
+
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+app.use(cors());
+
+// Middleware
+app.use(express.json());
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
+
+// Basic Routes
+app.get("/", (req, res) => {
+  res.send("Hello from Express Server");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is About Page");
+});
+
+// Use Routes
+app.use("/api", userRoutes);
+
+// Server Start
+const port = process.env.PORT ;
+
+app.listen(port, () => {
+  console.log(`Express server running on port ${port}`);
+});
